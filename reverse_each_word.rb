dba04e3f08d899113cc2d61ac636413ea5dc74c1
Rafael Garcia <rafagrcb@gmail.com>
2020-05-28 20:09:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 phrase = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-  array = phrase.split("")
-  return array = []
+  array = []
   array.each do |phrase|
     return array << phrase.reverse
   end
