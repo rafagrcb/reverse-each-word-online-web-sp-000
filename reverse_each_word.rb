153931efd24a9d1e1d7568b7d6fe225251df6e99
Rafael Garcia <rafagrcb@gmail.com>
2020-05-28 20:25:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,4 +10,10 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  
+  original_array = string.split(" ")
+  test_array = []
+  original_array.collect do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
